de57585f8bce200d212dceefe4b41081786e0082
Nastassia-A <nastassiaarkharova@gmail.com>
2020-10-15 14:24:20 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let newStr = '';
+    if (n % 10 === 0) {
+        n /= 10;
+    }
+    while (n > 0) {
+        newStr += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return newStr - 0;
 }
